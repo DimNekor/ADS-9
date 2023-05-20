043a3a48e35364c7955f3ed301b6ad9b679b14fe
DimNekor <117307225+DimNekor@users.noreply.github.com>
2023-05-20 11:24:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,33 @@
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
+#include <string>
+#include <cctype>
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  BST<std::string> tree;
+  int count = 0;
+  std::string tempStr = "";
+  bool flag = true;
+  bool flag2 = true;
+  while (!file.eof()) {
+    int ch = file.get();
+    flag2 = true;
+    if (flag) {
+      ch = tolower(ch);
+      flag = false;
+    }
+    if (ch == ' ' || ch == '\n' || ch == -1) {
+      flag = true;
+      tree.AddToken(tempStr);
+    }
+    if (ch == '.' || ch == ',' || ch == ':' || ch == ';' || ch == '(' ||
+        ch == ')' || ch == '!' || ch == '?' || ch == 'EOF')
+      flag2 = false;
+    if (flag2) tempStr += ch;
+    if (flag) tempStr = "";
+  }
+  file.close();
+  return tree;
 }
