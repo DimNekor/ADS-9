35b51e34bffe12df2741b39fb06f029a071de806
DimNekor <117307225+DimNekor@users.noreply.github.com>
2023-05-27 21:19:13 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,26 +11,16 @@
 BST<std::string> makeTree(const char* filename) {
   std::ifstream file(filename);
   BST<std::string> tree;
-  int count = 0;
   std::string tempStr = "";
-  bool flag = true;
-  bool flag2 = true;
+  char ch;
   while (!file.eof()) {
-    int ch = file.get();
-    flag2 = true;
-    if (flag) {
-      ch = tolower(ch);
-      flag = false;
-    }
-    if ((ch == ' ' || ch == '\n' || ch == -1) && tempStr != "") {
-      flag = true;
+    ch = file.get();
+    if (!isalpha(ch)) {
       tree.AddToken(tempStr);
+      tempStr = "";
+    } else {
+      tempStr += tolower(ch);
     }
-    if (ch == '.' || ch == ',' || ch == ':' || ch == ';' || ch == '(' ||
-        ch == ')' || ch == '!' || ch == '?' || ch == '-' || ch == '\"')
-      flag2 = false;
-    if (flag2) tempStr += ch;
-    if (flag) tempStr = "";
   }
   file.close();
   return tree;
